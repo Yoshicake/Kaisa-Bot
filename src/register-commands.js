340c293e83862a2461ec8a0b44a255781f58714a
Yoshicake <grant.allen0268@gmail.com>
2023-12-03 22:45:07 -0500

    Update register-commands.js
    Added fortnite and coin flip commands

--- src/register-commands.js
+++ src/register-commands.js
@@ -23,16 +23,109 @@ const commands = [
         ],
     },
     { 
-        name: 'track',
-        description: 'Sends a link to a user\'s tracker.gg profile. NA only.',
+        name: 'league',
+        description: 'Sends a link to a user\'s tracker.gg profile.',
         options: [
+            {
+                name: 'region',
+                description: 'Region the user plays in.',
+                type: ApplicationCommandOptionType.String,
+                required: true,
+                choices: [
+                    {
+                        name: 'Brazil',
+                        value: 'BR',
+                    },
+                    {
+                        name: 'Europe Nordic & East',
+                        value: 'EUNE',
+                    },
+                    {
+                        name: 'Europe West',
+                        value: 'EUW',
+                    },
+                    {
+                        name: 'Latin America North',
+                        value: 'LAN',
+                    },
+                    {
+                        name: 'Latin America South',
+                        value: 'LAS',
+                    },
+                    {
+                        name: 'North America',
+                        value: 'NA',
+                    },
+                    {
+                        name: 'Oceania',
+                        value: 'OCE',
+                    },
+                    {
+                        name: 'Russia',
+                        value: 'RU',
+                    },
+                    {
+                        name: 'Turkey',
+                        value: 'TR',
+                    },
+                    {
+                        name: 'Japan',
+                        value: 'JP',
+                    },
+                    {
+                        name: 'Korea',
+                        value: 'KR',
+                    },
+                    {
+                        name: 'Philippines',
+                        value: 'PH',
+                    },
+                    {
+                        name: 'Singapore',
+                        value: 'SG',
+                    },
+                    {
+                        name: 'Taiwan',
+                        value: 'TW',
+                    },
+                    {
+                        name: 'Thailand',
+                        value: 'TH',
+                    },
+                    {
+                        name: 'Vietnam',
+                        value: 'VN',
+                    },
+                    
+                ] 
+            },
             {
                 name: 'username',
                 description: 'User you want to search up.',
                 type: ApplicationCommandOptionType.String,
                 required: true,
             },
+            {
+                name: 'tag',
+                description: 'Tag of the user.',
+                type: ApplicationCommandOptionType.String,
+                required: true,
+            },
+
         ],
+    },
+    {
+        name: 'fortnite',
+        description: "Links to a player\'s tracker.gg profile",
+        options: [
+            {
+                name: 'username',
+                description: "Epic Games username.",
+                type: ApplicationCommandOptionType.String,
+                required: true,
+
+            }
+        ]
     },
     {
         name: 'rps',
@@ -55,6 +148,28 @@ const commands = [
                 {
                     name: 'scissors',
                     value: 'scissors',
+                },
+            ],
+            },
+        ],
+    },
+    {
+        name: 'flip',
+        description: "Flip a coin and Kai'sa will tell you what you got!",
+        options: [ 
+            {
+            name: 'move',
+            description: 'Choose your move!',
+            type: ApplicationCommandOptionType.String,
+            required: true,
+            choices: [
+                {
+                    name: 'heads',
+                    value: 'heads',
+                },
+                {
+                    name: 'tails',
+                    value: 'tails',
                 },
             ],
             },
